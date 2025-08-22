0b65c5f691b21f81dfa0272067463f691e78fa60
buzziologia <vinicios.buzzi@gmail.com>
2025-08-22 16:53:50 -0300

    arrumado erros da linguagem

--- exercicios/ex_07/exercicio_07.go
+++ exercicios/ex_07/exercicio_07.go
@@ -9,20 +9,22 @@ import (
 var a int = 10
 var b string = "10"
 var c int = 100
+var d string = "10"
+var e int = 10
 
 func main() {
 
 	// declaracoes falsas
 	fmt.Println("False declarations")
 	fmt.Println(a > c)
-	fmt.Println(a != a)
+	fmt.Println(a != e)
 	fmt.Println(a >= 100)
 
 	fmt.Println("")
 
 	// declaracoes verdadeiras
 	fmt.Println("True declarations")
-	fmt.Println(a <= a)
-	fmt.Println(b == b)
+	fmt.Println(a <= e)
+	fmt.Println(b == d)
 	fmt.Println(c > a)
 }
