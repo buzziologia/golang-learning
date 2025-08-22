24de6544ac4c05d16c9f60752b416e4f39aecba5
buzziologia <vinicios.buzzi@gmail.com>
2025-08-22 17:00:28 -0300

    add exercicio_08

---
+++ exercicios/ex_08/exercicio_08.go
@@ -0,0 +1,16 @@
+/*Crie constantes tipadas e não-tipadas.*/
+
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	tipado     int = 10
+	not_tipado     = 100
+)
+
+func main() {
+	fmt.Println(tipado, not_tipado)
+}
