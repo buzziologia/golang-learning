666d8bc0bc52bced9081b2dc4d61ef73f1f4b75b
buzziologia <vinicios.buzzi@gmail.com>
2025-08-22 17:21:31 -0300

    add exercicio_10

---
+++ exercicios/ex_10/exercicio_10.go
@@ -0,0 +1,15 @@
+/*Crie uma variável de tipo string utilizando uma raw string literal.*/
+
+package main
+
+import "fmt"
+
+func main() {
+	my_var := `
+	#####################################
+	ーーーーー　おはいお　ーーーーー
+	#####################################
+	`
+
+	fmt.Println(my_var)
+}
