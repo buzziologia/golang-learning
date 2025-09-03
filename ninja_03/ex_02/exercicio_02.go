a84a6b21b55b8605e3cfea3b766925588bc85b90
buzziologia <vinicios.buzzi@gmail.com>
2025-09-03 16:18:54 -0300

    add exercicio_02

---
+++ ninja_03/ex_02/exercicio_02.go
@@ -0,0 +1,26 @@
+/*Põe na tela: O unicode code point de todas as letras maiúsculas do alfabeto, três vezes cada.
+Por exemplo:
+    65
+        U+0041 'A'
+        U+0041 'A'
+        U+0041 'A'
+    66
+        U+0042 'B'
+        U+0042 'B'
+        U+0042 'B'
+    ...e por aí vai.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for x := 65; x <= 90; x++ {
+		for i := 0; i <= 2; i++ {
+			fmt.Printf("%#U\n", x)
+		}
+	}
+}
