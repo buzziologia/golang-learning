92f57fc62ed7c996a483168496bde9731c930b83
buzziologia <vinicios.buzzi@gmail.com>
2025-09-03 16:47:13 -0300

    add exercicio_07

---
+++ ninja_03/ex_07/exercicio_07.go
@@ -0,0 +1,20 @@
+/*Utilizando a solução anterior (ninja_03/ex_06/exercicio_06), adicione as opções else if e else.
+ */
+
+// novo exericio, só pq o anterior ja estava no contexto desse com if and else
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	idade := 17
+
+	if idade >= 18 {
+		fmt.Println("Maior de idade")
+	} else {
+		fmt.Println("Menor de idade")
+	}
+
+}
